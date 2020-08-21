a48f4d4f70708bbac04c10670e0b3f96d9f9456e
kimura <mkimura7878@gmail.com>
2020-08-21 13:26:06 +0900

    ヘッダーのリンクを対応させた

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,21 +2,39 @@
   <div>
     <v-navigation-drawer v-model="drawer" app>
       <v-list dense>
-        <v-list-item link v-for="menu in menus" :key="menu.title" :to="menu.url">
+        <v-list-item link @click="$vuetify.goTo(0)">
           <v-list-item-action>
-            <v-icon>{{ menu.icon }}</v-icon>
+            <v-icon>mdi-home</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>{{ menu.title }}</v-list-item-title>
+            <v-list-item-title>TOP</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
-        <v-list-item link>
+        <v-list-item link @click="$vuetify.goTo(href='#about')">
           <v-list-item-action>
-            <i class="devicon-github-plain-wordmark"></i>
+            <v-icon>mdi-account</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>GitHub</v-list-item-title>
+            <v-list-item-title>ABOUT</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link @click="$vuetify.goTo(href='#skills')">
+          <v-list-item-action>
+            <v-icon>mdi-pencil</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>SKILLS</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link @click="$vuetify.goTo(href='#workes')">
+          <v-list-item-action>
+            <v-icon>mdi-widgets</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>WORKES</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
@@ -35,10 +53,6 @@
       <v-btn text @click="$vuetify.goTo(href='#workes')">WORKES</v-btn>
 
       <v-spacer></v-spacer>
-      <v-btn text href="https://github.com/kimura78">
-        <i class="devicon-github-plain-wordmark"></i>
-      </v-btn>
-
       <v-switch class="mt-5" v-model="$vuetify.theme.dark" label="Dark"></v-switch>
     </v-app-bar>
   </div>
@@ -49,12 +63,6 @@
   export default {
     data() {
       return {
-        menus: [
-          { title: 'Top', icon: 'mdi-home', },
-          { title: 'ABOUT', icon: 'mdi-account', },
-          { title: 'SKILLS', icon: 'mdi-pencil', },
-          { title: 'WORKES', icon: 'mdi-widgets', },
-        ],
         drawer: false,
       }
     },
