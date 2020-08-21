198d63ac2f97c01dc6353a76e7f38db18286f15e
kimura <mkimura7878@gmail.com>
2020-08-21 10:16:26 +0900

    Headerコンポーネントを作成した

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,20 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <Header/>
     <v-main>
-      <HelloWorld/>
+      <h1>MAIN</h1>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Header,
   },
 
   data: () => ({
@@ -58,3 +22,15 @@ export default {
   }),
 };
 </script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Recursive:wght@300&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@300&display=swap');
+  
+  #app {
+    font-family: 'Recursive', 'M PLUS 1p', sans-serif;
+  }
+  i {
+    font-size: 1.7rem;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item link v-for="menu in menus" :key="menu.title" :to="menu.url">
+          <v-list-item-action>
+            <v-icon>{{ menu.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ menu.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <i class="devicon-github-plain-wordmark"></i>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>GitHub</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-switch v-model="$vuetify.theme.dark" label="Dark"></v-switch>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar dense app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-btn text v-model="drawer" @click="$vuetify.goTo(0)">TOP</v-btn>
+      <v-btn text @click="$vuetify.goTo(href='#about')">ABOUT</v-btn>
+      <v-btn text @click="$vuetify.goTo(href='#skills')">SKILLS</v-btn>
+      <v-btn text @click="$vuetify.goTo(href='#workes')">WORKES</v-btn>
+
+      <v-spacer></v-spacer>
+      <v-btn text href="https://github.com/kimura78">
+        <i class="devicon-github-plain-wordmark"></i>
+      </v-btn>
+
+      <v-switch class="mt-5" v-model="$vuetify.theme.dark" label="Dark"></v-switch>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menus: [
+        { title: 'Top', icon: 'mdi-home', },
+        { title: 'ABOUT', icon: 'mdi-account', },
+        { title: 'SKILLS', icon: 'mdi-pencil', },
+        { title: 'WORKES', icon: 'mdi-widgets', },
+      ],
+      drawer: false,
+    }
+  },
+}
+</script>
